c4fb1496d051953a7f8498e6e109adc6412de7d4
Syed Mohammad Abuzar <40357673+abubow@users.noreply.github.com>
2023-02-03 16:30:47 +0500

    Added enviroment variable for port

---
+++ initial/loadEnv.go
@@ -0,0 +1,15 @@
+package initial
+
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+func LoadEnv() {
+	// Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"github.com/a/json-app/initial"
+	"github.com/gin-gonic/gin"
+)
 
+func init() {
+	initial.LoadEnv()
+}
 func main() {
-	fmt.Println("Hello, World!")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run()
 }
